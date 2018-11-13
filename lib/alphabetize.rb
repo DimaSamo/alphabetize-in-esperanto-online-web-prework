20644af79c12683643bbfc6e54aad2244ec3a7a7
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-13 05:08:27 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,9 @@
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
+  sentences_array.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
